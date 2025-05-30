d4919c3c31d7942f1d368daa084268cc90dab066
John Samper <johnsamperse@gmail.com>
2025-05-30 01:24:09 +0000

    commit 2  added props to the Card component to fill out body text  hopefully i did this right haha :)

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -2,32 +2,19 @@ import React from "react";
 
 
 
-const Card = () => {
-	return (
-
-		<div className=" container d-flex justify-content-center align-items-center g-2 
-		">
-
-		<div className="card h-100 ">
-			<img
-				src="https://i.ytimg.com/vi/7eTJ8_m43PY/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AHUBoAC4AOKAgwIABABGF0gLSh_MA8=&rs=AOn4CLBme3ntirTI7h3ODVlBMYMV6SCROQ"
-				className="card-img-top"
-				alt="..."
-			/>
-			<div className="card-body">
-				<h5 className="card-title">Card Title</h5>
-				<p className="card-text">
-					This is a card with supporting text below as a natural lead-in.
-				</p>
-                <div className="card-footer w-100  d-flex justify-content-center">
-                  <button className="btn btn-outline-danger" type="submit">Search</button>
-                </div>
-			</div>
-		</div>
-		</div>
-  
-	);
-};
+const Card = ({ bodyText, style }) => (
+  <div className="card" style={style}>
+    <div className="card-body">
+	  <img src="https://i.ytimg.com/vi/7eTJ8_m43PY/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AHUBoAC4AOKAgwIABABGF0gLSh_MA8=&rs=AOn4CLBme3ntirTI7h3ODVlBMYMV6SCROQ" className="card-img-top"
+                alt="..."
+            />
+      <p className="card-text">{bodyText}</p>
+	  <div className="d-flex justify-content-center">
+      <button className="btn btn-outline-danger" type="submit">Search</button>
+	  </div>
+    </div>
+  </div>
+);
 
 
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -19,15 +19,24 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar/>
     <Home/>
-   <div className="container mt-4 p-3">
+   <div className="container mt-4 p-3 px-0 ">
   <div className="row row-cols-1 row-cols-md-4 g-5 justify-content-center">
-    <div className="col d-flex justify-content-center">git <Card style={{ width: "24rem" }} /></div>
-    <div className="col d-flex justify-content-center"><Card style={{ width: "24rem" }} /></div>
-    <div className="col d-flex justify-content-center"><Card style={{ width: "24rem" }} /></div>
-    <div className="col d-flex justify-content-center"><Card style={{ width: "24rem" }} /></div>
+    <div className="col d-flex justify-content-center mt-0 p-2">
+      <Card style={{ width: "24rem" }} bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet erat aliquam, pellentesque ex non, faucibus nisi. Donec varius blandit dui, et posuere dui porttitor at.." />
+    </div>
+    <div className="col d-flex justify-content-center mt-0 p-2">
+      <Card style={{ width: "24rem" }} bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet erat aliquam, pellentesque ex non, faucibus nisi. Donec varius blandit dui, et posuere dui porttitor at." />
+    </div>
+    <div className="col d-flex justify-content-center mt-0 p-2">
+      <Card style={{ width: "24rem" }} bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet erat aliquam, pellentesque ex non, faucibus nisi. Donec varius blandit dui, et posuere dui porttitor at." />
+    </div>
+    <div className="col d-flex justify-content-center mt-0 p-2 ">
+      <Card style={{ width: "24rem" }} bodyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet erat aliquam, pellentesque ex non, faucibus nisi. Donec varius blandit dui, et posuere dui porttitor at." />
+    </div>
   </div>
   </div>
     <Footer/>
+    
    
 
    
